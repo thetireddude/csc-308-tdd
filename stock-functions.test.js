6e0171e6e02e7fb3182389398fe89f5758ae1af8
tired_ <omaralim06@gmail.com>
2026-09-16 00:59:29 -0700

    feat: Add failing test case for checking empty portfolio (RED)

--- stock-functions.test.js
+++ stock-functions.test.js
@@ -1,7 +1,13 @@
-const { createPortfolio } = require("./stock-functions");
+const { createPortfolio, isEmpty, } = require("./stock-functions");
 
 test("a new portfolio starts empty", () => {
     const portfolio = createPortfolio();
 
     expect(portfolio).toEqual({});
+});
+
+test("a portfolio is empty", () => {
+    const portfolio = createPortfolio();
+
+    expect(isEmpty(portfolio)).toBe(true);
 });
