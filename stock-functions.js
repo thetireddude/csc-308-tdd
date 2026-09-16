f1b5ede658d7de9f1388bdfe9544f099b4bdd0ce
tired_ <omaralim06@gmail.com>
2026-09-16 01:22:47 -0700

    feat: Implement stock purchase (GREEN)

--- stock-functions.js
+++ stock-functions.js
@@ -6,6 +6,15 @@ function isEmpty(portfolio) {
     return Object.keys(portfolio).length === 0;
 }
 
+function purchase(portfolio, symbol, shares) {
+    if (!portfolio[symbol] | portfolio[symbol] === 0) {
+        portfolio[symbol] = shares
+    }
+    else {
+        portfolio[symbol] = portfolio[symbol] + shares
+    }
+}
+
 module.exports = {
-    createPortfolio, isEmpty
+    createPortfolio, isEmpty, purchase
 };
